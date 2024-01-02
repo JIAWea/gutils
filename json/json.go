1a5e5e40065723b2aacf7df44d0c9c6a0a0e2a85
ray <1552937000@qq.com>
2024-01-02 10:49:49 +0800

    json

--- json/json.go
+++ json/json.go
@@ -1,45 +1,47 @@
 package json
 
 import (
-    "io"
+	"io"
 
-    "github.com/json-iterator/go"
-    "github.com/json-iterator/go/extra"
+	// "github.com/bytedance/sonic"
+
+	"github.com/json-iterator/go"
+	"github.com/json-iterator/go/extra"
 )
 
 var j = jsoniter.Config{
-    MarshalFloatWith6Digits:       false,
-    EscapeHTML:                    true,
-    SortMapKeys:                   true,
-    UseNumber:                     true,
-    DisallowUnknownFields:         false,
-    TagKey:                        "",
-    OnlyTaggedField:               false,
-    ValidateJsonRawMessage:        true,
-    ObjectFieldMustBeSimpleString: false,
-    CaseSensitive:                 false,
+	MarshalFloatWith6Digits:       false,
+	EscapeHTML:                    true,
+	SortMapKeys:                   true,
+	UseNumber:                     true,
+	DisallowUnknownFields:         false,
+	TagKey:                        "",
+	OnlyTaggedField:               false,
+	ValidateJsonRawMessage:        true,
+	ObjectFieldMustBeSimpleString: false,
+	CaseSensitive:                 false,
 }.Froze()
 
 func init() {
-    extra.RegisterFuzzyDecoders()
+	extra.RegisterFuzzyDecoders()
 }
 
 func Marshal(v interface{}) ([]byte, error) {
-    return j.Marshal(v)
+	return j.Marshal(v)
 }
 
 func MarshalIndent(v interface{}, prefix, indent string) ([]byte, error) {
-    return j.MarshalIndent(v, prefix, indent)
+	return j.MarshalIndent(v, prefix, indent)
 }
 
 func Unmarshal(data []byte, v interface{}) error {
-    return j.Unmarshal(data, v)
+	return j.Unmarshal(data, v)
 }
 
 func NewEncoder(w io.Writer) *jsoniter.Encoder {
-    return j.NewEncoder(w)
+	return j.NewEncoder(w)
 }
 
 func NewDecoder(r io.Reader) *jsoniter.Decoder {
-    return j.NewDecoder(r)
+	return j.NewDecoder(r)
 }
